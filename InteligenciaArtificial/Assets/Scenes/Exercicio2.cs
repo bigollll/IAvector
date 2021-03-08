e3da20ec18c76f74a28221094d61686e86bfbb6f
bigollll <rodrigobego@uol.com.br>
2021-03-08 19:43:04 -0300

    Release

---
+++ InteligenciaArtificial/Assets/Scenes/Exercicio2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio2 : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 a = transform.position;
+        Vector3 b = transform.position;
+        transform.position = Vector3.MoveTowards(a, b, speed);
+    }
+}
---
+++ InteligenciaArtificial/Assets/Scenes/MovePlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody Rig;
+
+    private void Start()
+    {
+        Rig = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 Position = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Rig.velocity = Position * speed;
+
+    }
+}
